155c1b45191a0e76d5c5e7443731e70fda8d8111
jhoncee <junnelhiro@hiro-c.net>
2025-06-03 15:17:54 +0800

    s

---
+++ api/api1/Controllers/MQProvider.cs
@@ -0,0 +1,42 @@
+﻿using MassTransit.Scheduling;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api1.Controllers
+{
+    public class MQProvider
+    {
+        public async Task Config()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "testQueue",
+                                   durable: false,
+                                   exclusive: false,
+                                   autoDelete: false,
+                                   arguments: null);
+
+            var consumer =  new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+                return Task.CompletedTask; 
+            };
+
+
+            await channel.BasicConsumeAsync(queue: "testQueue",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
